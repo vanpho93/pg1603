1d88719ac06c629a77efb6a82fdc954d959cd8f9
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-13 21:06:50 +0700

    xong

--- db.js
+++ db.js
@@ -13,12 +13,46 @@ const pool = new pg.Pool(config);
 
 function query(sql, cb) {
     pool.connect((err, client, done) => {
-        if (err) return console.log('LOI: ', err);
+        if (err) return cb(err);
         client.query(sql, (errQuery, result) => {
-            if (errQuery) return console.log(errQuery);
-            cb(result);
+            if (errQuery) return cb(errQuery);
+            cb(undefined, result);
         });
     });
 }
 
-query('SELECT * FROM "TinTuc"', result => console.log(result.rows));
+function getAllData(cb) {
+    const sql = 'SELECT * FROM "TinTuc"';
+    query(sql, (err, result) => {
+        if (err) return cb(err);
+        cb(undefined, result);
+    });
+}
+
+function insertNews(title, desc, image, idVideo) {
+
+}
+
+function deleteNews(id) {
+
+}
+
+function updateNews(id, title, desc, image, idVideo) {
+
+}
+
+// class Tin {
+//     constructor(id, title, desc, image, idVideo) {
+
+//     }
+
+//     remove() {
+
+//     }
+
+//     static getAll() {
+
+//     }
+// }
+
+getAllData((err, result) => console.log(result.rows));
